e859ef29d329c5574d27b21d4b14a1eb4f3b5013
sergeyman <sergey_man@ukr.net>
2023-07-11 16:00:48 +0300

    Adding Floor, Player and PlayerBehaviour.cs game script put on Player

---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+/// <summary>
+/// Responsible for moving the player automatically and
+/// reciving input.
+/// </summary>
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerBehaviour : MonoBehaviour
+{
+    /// <summary>
+    /// A reference to the Rigidbody component
+    /// </summary>
+    private Rigidbody rb;
+    
+    [Tooltip("How fast the ball moves left/right")]
+    public float dodgeSpeed = 5;
+    
+    [Tooltip("How fast the ball moves forwards automatically")]
+    [Range(0, 10)]
+    public float rollSpeed = 5;
+    
+    /// <summary>
+    /// Use this for initialization
+    /// </summary>
+    void Start()
+    {
+        // Get access to our Rigidbody component
+        rb = GetComponent<Rigidbody>();
+    }
+    
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
+    void Update()
+    {
+        // Check if we're moving to the sideBuilding Your Game Chapter 1
+
+        var horizontalSpeed = Input.GetAxis("Horizontal") * dodgeSpeed;
+        // Apply our auto-moving and movement forces
+        rb.AddForce(horizontalSpeed, 0, rollSpeed);
+    }
+}
