1e8c9a5c50cbc490a03683ab7629ff62d48bb8b3
sergeyman <sergey_man@ukr.net>
2023-07-13 13:32:06 +0300

    Added Obstacles with ObstaclesBehaviour.cs and changes in GameController.cs for automatically creating obstacles during game flow at obstacles Spawn Points set in Basic Tile prefab. Need to make the creation of them more then maximum numbers set in initNoObstacles (?)

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections.Generic; // List
 
 /// <summary>
 /// Controls the main gameplay
@@ -8,12 +9,18 @@ public class GameController : MonoBehaviour
     [Tooltip("A reference to the tile we want to spawn")]
     public Transform tile;
 
+    [Tooltip("A reference to the obstacle we want to spawn")]
+    public Transform obstacle;
+
     [Tooltip("Where the first tile should be placed at")]
     public Vector3 startPoint = new Vector3(0, 0, -5);
 
     [Tooltip("How many tiles should we create in advance")]
     [Range(1, 15)]
     public int initSpawnNum = 10;
+
+    [Tooltip("How many tiles to spawn initially with no obstacles")]
+    public int initNoObstacles = 4;
 
     /// <summary>
     /// Where the next tile should be spawned at.
@@ -28,7 +35,6 @@ public class GameController : MonoBehaviour
     /// <summary>
     /// Used for initialization
     /// </summary>
-    /// 
     void Start()
     {
         // Set our starting point
@@ -37,7 +43,8 @@ public class GameController : MonoBehaviour
 
         for (int i = 0; i < initSpawnNum; ++i)
         {
-            SpawnNextTile();
+            // SpawnNextTile();
+            SpawnNextTile(i >= initNoObstacles);
         }
     }
     
@@ -54,4 +61,73 @@ public class GameController : MonoBehaviour
         nextTileLocation = nextTile.position;
         nextTileRotation = nextTile.rotation;
     }
+
+    /// <summary>
+    /// Will spawn a tile at a certain location and setup the next position
+    /// /// </summary>
+    /// <param name="spawnObstacles">If we should spawn an
+    /// obstacle</param>
+    public void SpawnNextTile(bool spawnObstacles = true)
+    {
+        var newTile = Instantiate(tile, nextTileLocation, nextTileRotation);
+
+        // Figure out where and at what rotation we should spawn
+        // the next item
+        var nextTile = newTile.Find("Next Spawn Point");
+        nextTileLocation = nextTile.position;
+        nextTileRotation = nextTile.rotation;
+
+        // 2017
+        if (!spawnObstacles)
+            return;
+
+        // 2020
+        //if (spawnObstacles)
+        //{
+        //    SpawnObstacle(newTile);
+        //}
+
+        // Now we need to get all of the possible places to spawn the
+        // obstacle
+        var obstacleSpawnPoints = new List<GameObject>();
+        
+        // Go through each of the child game objects in our tile
+        foreach (Transform child in newTile)
+        {
+            // If it has the ObstacleSpawn tag
+            if (child.CompareTag("ObstacleSpawn"))
+            {
+                // We add it as a possibilty
+                obstacleSpawnPoints.Add(child.gameObject);
+            }
+        }
+        
+        // Make sure there is at least one
+        if (obstacleSpawnPoints.Count > 0)
+        {
+            // Get a random object from the ones we have
+            var spawnPoint = obstacleSpawnPoints[Random.Range(0, obstacleSpawnPoints.Count)];
+            
+            // Store its position for us to use
+            var spawnPos = spawnPoint.transform.position;
+            
+            // Create our obstacle
+            var newObstacle = Instantiate(obstacle, spawnPos, Quaternion.identity);
+            
+            // Have it parented to the tile
+            newObstacle.SetParent(spawnPoint.transform);
+        }
+    }
 }
+
+
+/*
+ * 
+ * Marker spawn obstacles points
+ * Center:  0, 1, 4         // 1, -6, 4
+ * Left:    -2, 1, 4        // -1, -6, 4
+ * Rigth:   2, 1, 4         // 3, -6, 4
+ * 
+ * 
+ * 
+ */
--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -28,11 +28,12 @@ public class PlayerBehaviour : MonoBehaviour
         // Get access to our Rigidbody component
         rb = GetComponent<Rigidbody>();
     }
-    
+
     /// <summary>
     /// Update is called once per frame
     /// </summary>
-    void Update()
+    // void Update()
+    void FixedUpdate()
     {
         // Check if we're moving to the sideBuilding Your Game
 
@@ -41,3 +42,20 @@ public class PlayerBehaviour : MonoBehaviour
         rb.AddForce(horizontalSpeed, 0, rollSpeed);
     }
 }
+
+/*
+ *   функция Update вызывается один раз за кадр, на котором запущена игра.
+ *   Следует учитывать, что частота вызова функции Update является переменной,
+ *   то есть она может меняться с течением времени. Это зависит от ряда факторов,
+ *   в том числе от используемого аппаратного обеспечения.
+ *    This means that the more times the
+ *   Update function is called, the better the computer is. We want a consistent
+ *   experience for all of our players and one of the ways that we can do that is by
+ *   using the FixedUpdate function.
+ *   FixedUpdate похожа на Update с некоторыми ключевыми отличиями. Первое заключается в том, что она вызывается
+ *   через фиксированные временные интервалы, то есть в одно и то же время между вызовами. Также важно отметить, что
+ *   что после вызова FixedUpdate производятся физические расчеты. Это означает, что
+ *   код, изменяющий объекты, основанные на физике, должен выполняться внутри функции FixedUpdate
+ *   вообще, за исключением разовых событий, таких как прыжок
+ * 
+ */
